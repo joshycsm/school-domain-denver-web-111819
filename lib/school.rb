f23f1b508d915894c6b9c23724f7417a4a4bbb3c
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-25 06:02:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,13 +8,13 @@ class School
   end
   
   def add_student(student_name, grade)
-    roster[grade] || = []
-    roster[grade] << student_name
+    # roster[grade] || = []
+    # roster[grade] << student_name
   end
   
-  def grade(student_grade)
-    roster[student_grade]
-  end
+  # def grade(student_grade)
+  #   roster[student_grade]
+  # end
   
   
     
